d0c7261acd2d5faed5eeecbab1eee7272182f2bc
marco <marcoxmeijer@gmail.com>
2019-12-24 21:54:50 +0100

    added robustness

--- crawler.py
+++ crawler.py
@@ -3,10 +3,14 @@ import urllib.request
 import string
 import re
 from goodreads import client
+from goodreads.request import GoodreadsRequestException
 
 
 import sqlite3
 from sqlite3 import Error
+
+import time
+
 
 gc = client.GoodreadsClient("f4YcFGIKNiejrGv59xXQ", "WO8By9ua8KPF9x9c9PiPaAhYSwzGjhqB0JEVgos")
 
@@ -83,25 +87,35 @@ def generate_list(localsoup):
     table =  localsoup.table
     table_rows=table.find_all('tr')
     for tr in table_rows:
-        goodreads_id=tr.find('div',class_='u-anchorTarget')['id']
-        #name=tr.find('a',class_="bookTitle").find('span').text
-        author=tr.find('a',class_="authorName").find('span',itemprop="name").text
+        exceptcount=4
+        while exceptcount > 0:
+            try:
+                goodreads_id=tr.find('div',class_='u-anchorTarget')['id']
+                #name=tr.find('a',class_="bookTitle").find('span').text
+                author=tr.find('a',class_="authorName").find('span',itemprop="name").text
 
-        #ratingtext=re.sub(r'([a-zA-Z,])','',tr.find('span', class_="minirating").text).split("—")
-        #rating=float(ratingtext[0])
-        #ratingcount=int(ratingtext[1])
+                #ratingtext=re.sub(r'([a-zA-Z,])','',tr.find('span', class_="minirating").text).split("—")
+                #rating=float(ratingtext[0])
+                #ratingcount=int(ratingtext[1])
+                print(goodreads_id)
+                book=gc.book(goodreads_id)
+                name=book.title
+                #author=book.authors()
+                rating=book.average_rating
+                ratingcount=book.ratings_count
+                ratingdist=book.rating_dist
+                text_reviews=book.text_reviews_count
+                date=str(book.publication_date[2])+"-"+str(book.publication_date[0])+"-"+str(book.publication_date[1])
 
-        book=gc.book(goodreads_id)
-        name=book.title
-        #author=book.authors()
-        rating=book.average_rating
-        ratingcount=book.ratings_count
-        ratingdist=book.rating_dist
-        text_reviews=book.text_reviews_count
-        date=str(book.publication_date[2])+"-"+str(book.publication_date[0])+"-"+str(book.publication_date[1])
+                entry=(goodreads_id,name,author,rating,ratingcount,ratingdist,text_reviews,date)
+                add_fantasy_entry(connextion,entry)
+                exceptcount=0
+            except GoodreadsRequestException:
+                exceptcount=exceptcount-1
+                print("encountered goodreadsexception")
+                time.sleep(3)
 
-        entry=(goodreads_id,name,author,rating,ratingcount,ratingdist,text_reviews,date)
-        add_fantasy_entry(connextion,entry)
+
 
         #print(entry)
 
@@ -120,6 +134,7 @@ def parse_list(url):
         sauce = urllib.request.urlopen(newUrl).read()
         soup = bs.BeautifulSoup(sauce,'lxml')
         generate_list(soup)
+        connextion.commit() #commit once per page, don't want too many writes on our file system
     
 
 
@@ -132,4 +147,4 @@ with open(filepath) as fp:
         line = fp.readline().replace('\n','')
         cnt += 1
 
-connextion.commit()
+
